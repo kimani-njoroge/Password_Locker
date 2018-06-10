4293be3a645cf4afbefddf803f6d2453829359e2
kimani-njoroge <kimani064@gmail.com>
2018-06-10 22:12:25 +0300

    append username and password to file storage

--- lock.py
+++ lock.py
@@ -13,7 +13,14 @@ class User:
         print('Creating user, please choose username and password')
         username = input('Username: ')
         password = input('Password: ')
-    
+
+        file = open("mastcred.txt","a")
+        file.write(username)
+        file.write(" ")
+        file.write(password)
+        file.write("\n")
+        file.close()
+
 
 class Credentials:
     """
