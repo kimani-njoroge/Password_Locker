68baefdb2ae8e10d4b77cde409c671f2ac5f5acf
kimani-njoroge <kimani064@gmail.com>
2018-06-10 22:13:36 +0300

    create loop to call short codes

--- lock.py
+++ lock.py
@@ -2,6 +2,18 @@ def all():
     '''
     This function holds all data methods
     '''
+    print("log-for old user","new-new user","newc-new credentials","viewc-view credentials")
+    calls = input("call: ")
+    if calls == "new":
+        user.add_user()
+        all()
+    elif calls == "log":
+        login()
+        cred.create_credentials()
+    elif calls == "viewc":
+        login()
+        cred.show_credentials()
+
 
 class User:
     """
